302c8d0e6a4833aeffeb2aebc49549b1e9b7c11d
Taillan <math.tail@outlook.fr>
2025-02-08 17:16:12 +0100

    suffix security groups

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -1,5 +1,5 @@
-resource "aws_security_group" "EC2_LinuxJumpServer" {
-    name        = "EC2_LinuxJumpServer"
+resource "aws_security_group" "EC2_LinuxJumpServer_sg" {
+    name        = "EC2_LinuxJumpServer_sg"
     description = "Security group for EC2 Linux Jump Server"
     vpc_id      = aws_vpc.EKS_VPC.id
 
@@ -10,8 +10,8 @@ resource "aws_security_group" "EC2_LinuxJumpServer" {
     }
 }
 
-resource "aws_security_group" "EKS_Cluster" {
-    name        = "EKS_Cluster"
+resource "aws_security_group" "EKS_Cluster_sg" {
+    name        = "EKS_Cluster_sg"
     description = "Security group for EKS Cluster"
     vpc_id      = aws_vpc.EKS_VPC.id
 
@@ -22,8 +22,8 @@ resource "aws_security_group" "EKS_Cluster" {
     }
 }
 
-resource "aws_security_group" "ECR" {
-    name        = "ECR"
+resource "aws_security_group" "ECR_sg" {
+    name        = "ECR_sg"
     description = "Security group for ECR"
     vpc_id      = aws_vpc.EKS_VPC.id
 
@@ -34,8 +34,8 @@ resource "aws_security_group" "ECR" {
     }
 }
 
-resource "aws_security_group" "EC2_Jenkins_Master" {
-    name        = "EC2_Jenkins_Master"
+resource "aws_security_group" "EC2_Jenkins_Master_sg" {
+    name        = "EC2_Jenkins_Master_sg"
     description = "Security group for EC2 Jenkins Master"
     vpc_id      = aws_vpc.EKS_VPC.id
 
@@ -46,8 +46,8 @@ resource "aws_security_group" "EC2_Jenkins_Master" {
     }
 }
 
-resource "aws_security_group" "EC2_Jenkins_Slave" {
-    name        = "EC2_Jenkins_Slave"
+resource "aws_security_group" "EC2_Jenkins_Slave_sg" {
+    name        = "EC2_Jenkins_Slave_sg"
     description = "Security group for EC2 Jenkins Slave"
     vpc_id      = aws_vpc.EKS_VPC.id
 
